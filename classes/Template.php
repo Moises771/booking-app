858ee0072b7e7ab3f33f88885800eac16ebf2eee
Moises771 <Julianbrb23@gmail.com>
2023-01-01 20:16:07 +0100

    rules page and template

---
+++ classes/Template.php
@@ -0,0 +1,26 @@
+<?php
+
+class Template
+{
+
+    public static function header($title)
+    {?>
+        <!DOCTYPE html>
+        <html lang="en">
+
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title><?=$title?> - Football Near You</title>
+        </head>
+
+        <body>
+            <h1><?=$title?></h1>
+            <nav>
+                <a href="/booking-app/pages/courts.php">Courts</a>
+                <a href="/booking-app/pages/login.php">Login</a>
+                <a href="/booking-app/pages/register.php">Register</a>
+            </nav>
+    <?php }
+}
--- index.php
+++ index.php
@@ -1,23 +1,9 @@
 <?php
 require_once __DIR__ . "/classes/Database.php";
+require_once __DIR__ . "/classes/Template.php";
 
-$db = new Database();
+$db = new Database();Template:: header("football near you");
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
-</head>
-<body>
-    <h1>Football Near You</h1>
-    <nav>
-        <a href="/booking-app/pages/courts.php">Courts</a>
-        <a href="/booking-app/pages/login.php">Login</a>
-        <a href="/booking-app/pages/register.php">Register</a>
-    </nav>
-</body>
-</html>
+
+
