2249f8ef36020c3379d0d8df20747d28d0334a48
Moises771 <Julianbrb23@gmail.com>
2023-01-04 18:01:53 +0100

    added style and edited navbar

--- classes/Template.php
+++ classes/Template.php
@@ -21,11 +21,12 @@ class Template
             <meta http-equiv="X-UA-Compatible" content="IE=edge">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <title><?= $title ?> - Football Near You</title>
+            <link rel="stylesheet" href="/booking-app/assets/styles.css">
         </head>
 
         <body>
             <h1><?= $title ?></h1>
-            <nav>
+            <nav class="navMenu">
                 <a href="/booking-app">Home</a>
                 <a href="/booking-app/pages/courts.php">Courts</a>
 
